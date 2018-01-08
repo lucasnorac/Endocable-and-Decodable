2564d2d307a3392dfd20f7b2caeff03429af4d74
lucasnorac <lucas_albarello_92@hotmail.com>
2018-01-07 22:26:50 -0200

    TerminarAmanha

---
+++ Encodable and Decodable/Post.swift
@@ -0,0 +1,16 @@
+//
+//  Post.swift
+//  Encodable and Decodable
+//
+//  Created by Lucas Caron Albarello on 07/01/2018.
+//  Copyright © 2018 Lucas Caron Albarello. All rights reserved.
+//
+
+import Foundation
+
+struct Post {
+    let corpo : String
+    let id: Int
+    let titulo: String
+    let usuarioId: Int
+}
--- Encodable and Decodable/ViewController.swift
+++ Encodable and Decodable/ViewController.swift
@@ -16,11 +16,23 @@ class ViewController: UIViewController {
         var request = URLRequest(url: url)
         request.httpMethod = "POST"
         request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        do {
-            
-        }
+        let postDictionary = ["userId":"55", "id":"126", "title":"Hey they", "body":"What are you doing?"]
+        let newPost = Post(corpo: <#T##String#>, id: <#T##Int#>, titulo: <#T##String#>, usuarioId: <#T##Int#>)
+        do{
+            let json = try JSONSerialization.data(withJSONObject: postDictionary, options: [])
+            request.httpBody = json
+        } catch{}
         let session = URLSession.shared
+        let task = session.dataTask(with: request) { (data, response, error) in
+            guard let data = data else {return}
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            }catch{}
+        }
+    task.resume()
     }
+    
     @IBAction func getUsuarios(_ sender: UIButton) {
         
     }
